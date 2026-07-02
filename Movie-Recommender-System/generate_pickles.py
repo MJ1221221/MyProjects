3792ca09e4522bb0baa761006226135376ee3c28
Mayank Jain <jainmayank0145@gmail.com>
2026-07-02 09:19:06 +0530

    some new changes

---
+++ Movie-Recommender-System/generate_pickles.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import ast
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import os
+
+os.makedirs('model', exist_ok=True)
+
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+movies = movies.merge(credits, on='title')
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+movies.dropna(inplace=True)
+
+
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+
+def convert3(obj):
+    L = []
+    counter = 0
+    for i in ast.literal_eval(obj):
+        if counter != 3:
+            L.append(i['name'])
+            counter += 1
+        else:
+            break
+    return L
+
+
+def fetch_director(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+            break
+    return L
+
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(convert3)
+movies['crew'] = movies['crew'].apply(fetch_director)
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['crew'] = movies['crew'].apply(lambda x: [i.replace(" ", "") for i in x])
+
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+new_df = movies[['movie_id', 'title', 'tags']]
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
+
+ps = PorterStemmer()
+
+
+def stem(text):
+    y = []
+    for i in text.split():
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+
+new_df['tags'] = new_df['tags'].apply(stem)
+new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+similarity = cosine_similarity(vectors)
+
+pickle.dump(new_df, open('model/movie_list.pkl', 'wb'))
+pickle.dump(similarity, open('model/similarity.pkl', 'wb'))
+print("Pickle files generated successfully!")
